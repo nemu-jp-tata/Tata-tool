c7e65a2ef31cfda71428343ee3a327f4c5819325
nemu-jp-tata <ootomo.koji@gmail.com>
2026-09-10 17:53:04 +0900

    Create imageList.js

---
+++ imageList.js
@@ -0,0 +1,16 @@
+const imagesFolderList = [
+  "P_beropaka.webp", "P_bilijika.webp", "P_bililimori.webp", "P_bilipiyo.webp",
+  "P_erekineko.webp", "P_furebi.webp", "P_gaoden.webp", "P_greenbeetle.webp",
+  "P_himawarin.webp", "P_himori.webp", "P_hinyao.webp", "P_horurin.webp",
+  "P_hukurogumo.webp", "P_kopanda.webp", "P_koroju.webp", "P_korokame.webp",
+  "P_korotama.webp", "P_maroshu.webp", "P_moemin.webp", "P_mogurin.webp",
+  "P_nemukurage.webp", "P_purabi.webp", "P_sizukumusi.webp", "P_tubutumuri.webp",
+  "P_アタタマ.webp", "P_ウミミ.webp", "P_コマキリ.webp", "P_コロコン.webp",
+  "P_コワガルー.webp", "P_サボール.webp", "P_スケダコ.webp", "P_ツヨカニ.webp",
+  "P_トコペンギン.webp", "P_トジコモル.webp", "P_トラーニー.webp", "P_ナミアシカ.webp",
+  "P_ヌスケ.webp", "P_ネコオリ.webp", "P_パクマ.webp", "P_ヒノムシ.webp",
+  "P_ヒバイヌ.webp", "P_ピカル.webp", "P_フグマル.webp", "P_フリコー.webp",
+  "P_フルッグ.webp", "P_ププンク.webp", "P_ホネギョ.webp", "P_ボウズヘビ.webp",
+  "P_ポタカゲ.webp", "P_ムムカバ.webp", "P_ヤミノメ.webp", "P_ヤンザル.webp",
+  "P_ライメー.webp", "P_ライロウ.webp", "P_ワンブー.webp"
+];
