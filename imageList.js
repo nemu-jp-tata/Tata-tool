bc97fd6e55aee0191488ec2f367e7b93df0d0d5d
nemu-jp-tata <ootomo.koji@gmail.com>
2026-09-12 01:14:25 +0900

    Update imageList.js

--- imageList.js
+++ imageList.js
@@ -4,7 +4,7 @@ const imagesFolderList = [
   "P_ヒマワリン.webp", "P_ヒモリ.webp", "P_ヒニャオ.webp", "P_horurin.webp",
   "P_hukurogumo.webp", "P_コパンダ.webp", "P_koroju.webp", "P_korokame.webp",
   "P_korotama.webp", "P_maroshu.webp", "P_moemin.webp", "P_mogurin.webp",
-  "P_ネムクラゲ.webp", "P_purabi.webp", "P_sizukumusi.webp", "P_tubutumuri.webp",
+  "P_ネムクラゲ.webp", "P_purabi.webp", "P_シズクジ.webp", "P_tubutumuri.webp",
   "P_アタタマ.webp", "P_ウミミ.webp", "P_コマキリ.webp", "P_コロコン.webp",
   "P_コワガルー.webp", "P_サボール.webp", "P_スケダコ.webp", "P_ツヨカニ.webp",
   "P_トコペンギン.webp", "P_トジコモル.webp", "P_トラーニー.webp", "P_ナミアシカ.webp",
